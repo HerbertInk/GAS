63ac4c9aa8c4a41b3090e09c24b1f28b9460acef
HerbertInk <herbertn200@gmail.com>
2023-07-21 01:48:40 +0300

    demo sales

---
+++ demo/gasinnit.py
@@ -0,0 +1,34 @@
+# variables innit
+total_sales = 0
+total_debts = 0
+total_debt_payments = 0
+
+# gas products
+gas_products = {'Gasoline': {'price_per_unit': 0, 'total_units_sold': 0, 'debts': 0, 'debt_payments': 0},
+                'Diesel': {'price_per_unit': 0, 'total_units_sold': 0, 'debts': 0, 'debt_payments': 0},
+                'Kerosene': {'price_per_unit': 0, 'total_units_sold': 0, 'debts': 0, 'debt_payments': 0}}
+
+# looping through each gas product
+
+for product in gas_products:
+
+    # get input from user for current gas product
+    gas_products[product]['price_per_unit'] = float(input(f"Unit price {product}: "))
+    gas_products[product]['total_units_sold'] = float(input(f"Units sold {product}: "))
+    gas_products[product]['debts'] = float(input(f"Debts {product}: "))
+    gas_products[product]['debt_payments'] = float(input(f"Payments {product}: "))
+
+    # calculate sales for the current gas product
+    sales = gas_products[product]['price_per_unit'] * gas_products[product]['total_units_sold']
+    total_sales += sales
+    total_debts += gas_products[product]['debts']
+    total_debt_payments += gas_products[product]['debt_payments']
+
+    # sales for the current gas product
+    print(f"Sales for {product}: {sales:.2f} dollars")
+
+# print total sales, debts, and debt payments for the day
+print("\n SALES FOR THE DAY \n")
+print(f"Debts for the day: ${total_debts:.2f}")
+print(f"Payments: ${total_debt_payments:.2f}")
+print(f"Sales for the day: ${total_sales:.2f}")
